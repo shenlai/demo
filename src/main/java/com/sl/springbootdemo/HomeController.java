c4f092a28949932bcff678933e53820877ec9956
shenlai <shlai244@163.com>
2018-07-29 23:31:08 +0800

    spring boot demo

---
+++ src/main/java/com/sl/springbootdemo/HomeController.java
@@ -0,0 +1,13 @@
+package com.sl.springbootdemo;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/demo")
+    public String demo() {
+        return "Spring Boot Demo 修改";
+    }
+
+}
---
+++ src/main/java/com/sl/springbootdemo/SpringbootdemoApplication.java
@@ -0,0 +1,13 @@
+package com.sl.springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootdemoApplication {
+
+	public static void main(String[] args) {
+		System.out.print("23424riuwriwer");
+		SpringApplication.run(SpringbootdemoApplication.class, args);
+	}
+}
