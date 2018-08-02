25412a11573c7c8607825368b322e0b594312134
shenlai <shlai244@163.com>
2018-08-02 18:54:37 +0800

    配置读取

--- src/main/java/com/sl/springbootdemo/HomeController.java
+++ src/main/java/com/sl/springbootdemo/HomeController.java
@@ -1,16 +1,48 @@
 package com.sl.springbootdemo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.env.Environment;
 
 @RestController
 public class HomeController {
 
+    @Autowired
+    private Environment env;
 
     @RequestMapping("/demo")
     public String demo() {
-        EnvTestConfig config = new EnvTestConfig();
-        System.out.print(config.getServerPort());
+
+
+        System.out.print(env.getProperty("server.port",Integer.class));
         return "Spring Boot Demo 修改";
     }
+
+
+    @Value("${server.port}")
+    private String port;
+
+    @Value("${server.title:默认值}")
+    private String title;
+
+
+    @RequestMapping("/demo2")
+    public String demo2() {
+        System.out.print("端口号:"+port);
+        System.out.print("title:"+title);
+        return "Spring Boot Demo 修改";
+    }
+
+    @Autowired
+    private PropertiesConfig config;
+    @RequestMapping("/demo3")
+    public String demo3() {
+        System.out.print(config.getName());
+        System.out.print(config.getPassword());
+        return "Spring Boot Demo";
+    }
+
 
 }
