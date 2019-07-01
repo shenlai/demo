eec8875db4ba4a9ee94793a551d79274bc352623
shenlai <shenlai@lvmama.com>
2019-07-01 20:18:41 +0800

    canal client

---
+++ springboot-canalclient/src/main/java/com/sl/springbootcanalclient/SpringbootCanalclientApplication.java
@@ -0,0 +1,13 @@
+package com.sl.springbootcanalclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCanalclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootCanalclientApplication.class, args);
+    }
+
+}
