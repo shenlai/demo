6b9266dffa284dc80c3c84b132701501eeebf09f
shenlai <shlai244@163.com>
2020-02-01 20:40:56 +0800

    init

---
+++ lessmore/src/main/java/com/lessmore/front/LessmoreFrontApplication.java
@@ -0,0 +1,13 @@
+package com.lessmore.front;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LessmoreFrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LessmoreFrontApplication.class, args);
+    }
+
+}
---
+++ lessmore/src/test/java/com/lessmore/front/LessmoreFrontApplicationTests.java
@@ -0,0 +1,11 @@
+package com.lessmore.front;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LessmoreFrontApplicationTests {
+
+    void contextLoads() {
+    }
+
+}
