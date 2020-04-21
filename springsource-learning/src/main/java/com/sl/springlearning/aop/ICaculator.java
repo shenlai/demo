9f35976119994abc3afbd857d38ebfed74f85097
shenlai <shlai244@163.com>
2020-04-21 22:10:05 +0800

    AOP

---
+++ springsource-learning/src/main/java/com/sl/springlearning/aop/ICaculator.java
@@ -0,0 +1,5 @@
+package com.sl.springlearning.aop;
+
+public interface ICaculator {
+    int div(int i, int j);
+}
--- springsource-learning/src/main/java/com/sl/springlearning/aop/MathCaculatorV2.java
+++ springsource-learning/src/main/java/com/sl/springlearning/aop/MathCaculatorV2.java
@@ -4,11 +4,11 @@ package com.sl.springlearning.aop;
 /**
  * CGLIB 代理
  */
-public class MathCaculator {
+public class MathCaculatorV2 implements ICaculator {
 
 
     public int div(int i, int j) {
-        System.out.println("MathCalculator...div...");
+        System.out.println("MathCaculatorV2...div...");
         return i / j;
     }
 }
