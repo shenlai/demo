8e9da88ddb5667fa5c348219f59d6ac58dac89c7
shenlai <sl1001453>
2021-04-09 15:20:57 +0800

    temp

---
+++ demo-es/src/test/java/com/le/es/TestControllerTest.java
@@ -0,0 +1,29 @@
+package com.le.es;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.Assert;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author shenlai
+ * @Description TODO
+ * @create 2021/4/9 15:18
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = TestControllerTest.class)
+public class TestControllerTest {
+
+    @Test
+    public void testAuditMaterials() {
+
+       // Assert.isTrue(apiResponse.getCode() == 200, "");
+    }
+
+
+}
