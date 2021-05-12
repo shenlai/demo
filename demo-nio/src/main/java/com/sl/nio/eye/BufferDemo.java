694e1c1d5ac74d70dfb9496653a735ea562968e9
沈来 <sl1001453>
2021-05-12 17:25:26 +0800

    temp

--- demo-nio/src/main/java/com/sl/nio/eye/BufferDemo.java
+++ demo-nio/src/main/java/com/sl/nio/eye/BufferDemo.java
@@ -12,6 +12,18 @@ import java.nio.channels.FileChannel;
  */
 public class BufferDemo {
 
+
+    //数据是从通道读入缓冲区，从缓冲区写入到通道中的
+    public static void main(String[] args) throws Exception {
+        //new BufferDemo().testBuffer();
+
+        //new BufferDemo().testWriteBuffer();
+
+        new BufferDemo().testReadBuffer();
+
+
+    }
+
     /**
      * 调用clear()或compact()方法。clear()方法会清空整个缓冲区。compact()方法只会清除已经读过的数据
      * @throws Exception
@@ -38,13 +50,77 @@ public class BufferDemo {
         aFile.close();
     }
 
-    //数据是从通道读入缓冲区，从缓冲区写入到通道中的
-    public static void main(String[] args) throws Exception {
-       new BufferDemo().testBuffer();
 
 
 
+    /**
+     * 写数据到Buffer有两种方式：
+     *
+     * 1: 从Channel写到Buffer。
+     *          通过Buffer的put()方法写到Buffer里。
+     *
+     * 2:   通过put方法写Buffer的例子：
+     *      buf.put(127);
+     *
+     * @throws Exception
+     */
+    public void testWriteBuffer() throws Exception {
+        RandomAccessFile aFile = new RandomAccessFile("D:\\textdata.json", "rw");
+        FileChannel inChannel = aFile.getChannel();
+        //create buffer with capacity of 48 bytes
+        ByteBuffer buf = ByteBuffer.allocate(4);
+        //read into buffer.
+        int bytesRead = inChannel.read(buf);
+        while (bytesRead != -1) {
+            System.out.println("Read " + bytesRead);
+            //make buffer ready for read
+            buf.flip();
+            while (buf.hasRemaining()) {
+                // read 1 byte at a time
+                System.out.print((char) buf.get());
+            }
+            //make buffer ready for writing
+            buf.clear();
+
+            //通过put方法写Buffer的例子：
+            buf.put(new byte[1]);
+            bytesRead = inChannel.read(buf);
+        }
+        aFile.close();
     }
 
 
+    /**
+     * 从Buffer中读取数据有两种方式：
+     * 1: 从Buffer读取数据到Channel。
+     * 2: 使用get()方法从Buffer中读取数据。
+     * @throws Exception
+     */
+    public void testReadBuffer() throws Exception {
+        RandomAccessFile aFile = new RandomAccessFile("D:\\textdata.json", "rw");
+        FileChannel inChannel = aFile.getChannel();
+        //create buffer with capacity of 48 bytes
+        ByteBuffer buf = ByteBuffer.allocate(4);
+        //read into buffer.
+        int bytesRead = inChannel.read(buf);
+        while (bytesRead != -1) {
+            System.out.println("Read " + bytesRead);
+            buf.flip();
+
+            //1:从Buffer读取数据到Channel的例子
+            int bytesWritten = inChannel.write(buf);
+
+            while (buf.hasRemaining()) {
+                //2: 使用get()方法从Buffer中读取数据。
+                System.out.print((char) buf.get());
+            }
+            //make buffer ready for writing
+            buf.clear();
+
+            //通过put方法写Buffer的例子：
+            buf.put(new byte[1]);
+            bytesRead = inChannel.read(buf);
+        }
+        aFile.close();
+    }
 }
